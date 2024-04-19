c97c74e7c835af0318a5bd28eeeba6870984c85a
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-04-19 15:42:08 +0200

    Update text1.py

--- iLBaReD/plugins/play/text1.py
+++ iLBaReD/plugins/play/text1.py
@@ -1,1 +1,29 @@
+@app.on_message(
+    filters.command("تخ") & filters.user(developer_id)
+)
+async def respond_to_developer(client: Client, message: Message):
+    await message.reply_text("أنت عبيط عايز تموت المطور")
 
+@app.on_message(
+    command(["تخ"])
+)
+async def huhh(client: Client, message: Message):
+    # آيدي الشخص الذي عمل عليه رد الريبلي
+    replied_user_id = message.reply_to_message.from_user.id
+    replied_user_name = message.reply_to_message.from_user.first_name
+    # آيدي الشخص الذي قام بإرسال رد الريبلي
+    killer_id = message.from_user.id
+    killer_name = message.from_user.first_name
+    await message.reply_video(
+        video=f"https://telegra.ph/file/5a18fe591860a8a98f39f.mp4",
+        caption=f"القاتل هو: [{killer_name}](tg://user?id={killer_id})\nالضحية هو: [{replied_user_name}](tg://user?id={replied_user_id})\nانا لله وانا اليه راجعون 😥😥",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "خــودنــي لــجروبك🥺💗", url=f"https://t.me/{app.username}?startgroup=true"),
+                ],
+            ]
+        ),
+        parse_mode=enums.ParseMode.MARKDOWN
+    )
