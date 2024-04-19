9cd1e39d96dab1b4572190ceb5a0fabba0a3a443
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-04-19 13:41:14 +0200

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/The-Old-Fazar/The-SuoRce-MuoSic-AdRenalen-iLBaRed",
+    "https://github.com/The-Old-Fazar/The-SouRce-AdRenalen-test",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
