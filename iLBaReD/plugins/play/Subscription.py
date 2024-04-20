3a413cdf4eb27658df851f21df08c01cf8dc30e2
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-04-20 09:41:27 +0200

    Update Subscription.py

--- iLBaReD/plugins/play/Subscription.py
+++ iLBaReD/plugins/play/Subscription.py
@@ -3,9 +3,9 @@ from pyrogram.types import Message
 from pyrogram.types import InlineKeyboardMarkup as Markup, InlineKeyboardButton as Button
 from pyrogram.enums import ChatType
 from pyrogram.errors import UserNotParticipant
-from AnonXMusic import app
+from iLBaReD import app
 
-channel = "BENfiles"
+channel = "WA_ADRENALEN"
 async def subscription(_, __: Client, message: Message):
     user_id = message.from_user.id
     try: await app.get_chat_member(channel, user_id)
