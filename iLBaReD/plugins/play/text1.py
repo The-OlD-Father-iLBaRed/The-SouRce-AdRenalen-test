02d07d988fd96bc2af92df72a97f723e3aac25d9
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-04-21 14:49:48 +0200

    Update text1.py

--- iLBaReD/plugins/play/text1.py
+++ iLBaReD/plugins/play/text1.py
@@ -15,8 +15,6 @@ stopProcess = False
 
 # ------------------------------------------------------------------------------- #
 
-
-        
 @app.on_message(filters.command(["مسح الحسابات المحذوفة"], ""))
 async def remove(client, message):
   global stopProcess
@@ -72,3 +70,75 @@ async def remove(client, message):
       await message.reply("↢ ما يمديك تستخدم هالأمر أنت مو مُشرف")  
   except FloodWait as e:
     await asyncio.sleep(e.value)                               
+        
+
+# ------------------------------------------------------------------------------- #
+
+@app.on_message(filters.command(["الادمنية"], ""))
+async def admins(client, message):
+  try: 
+    adminList = []
+    ownerList = []
+    async for admin in app.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+      if admin.privileges.is_anonymous == False:
+        if admin.user.is_bot == True:
+          pass
+        elif admin.status == ChatMemberStatus.OWNER:
+          ownerList.append(admin.user)
+        else:  
+          adminList.append(admin.user)
+      else:
+        pass   
+    lenAdminList= len(ownerList) + len(adminList)  
+    text2 = f"**طاقم المجموعة - {message.chat.title}**\n\n"
+    try:
+      owner = ownerList[0]
+      if owner.username == None:
+        text2 += f"↢ المالك\n└ {owner.mention}\n\n↢ الأدمنية\n"
+      else:
+        text2 += f"↢ المالك\n└ @{owner.username}\n\n↢ الأدمنية\n"
+    except:
+      text2 += f"↢المالك\n└ <i>Hidden</i>\n\n↢ الأدمنية\n"
+    if len(adminList) == 0:
+      text2 += "└ <i>↢المشرفين المخفيين</i>"  
+      await app.send_message(message.chat.id, text2)   
+    else:  
+      while len(adminList) > 1:
+        admin = adminList.pop(0)
+        if admin.username == None:
+          text2 += f"├ {admin.mention}\n"
+        else:
+          text2 += f"├ @{admin.username}\n"    
+      else:    
+        admin = adminList.pop(0)
+        if admin.username == None:
+          text2 += f"└ {admin.mention}\n\n"
+        else:
+          text2 += f"└ @{admin.username}\n\n"
+      text2 += f"↢ عدد الأدمنيـة**: {lenAdminList}"  
+      await app.send_message(message.chat.id, text2)           
+  except FloodWait as e:
+    await asyncio.sleep(e.value)       
+
+# ------------------------------------------------------------------------------- #
+
+@app.on_message(filters.command(["البوتات"], ""))
+async def bots(client, message):  
+  try:    
+    botList = []
+    async for bot in app.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.BOTS):
+      botList.append(bot.user)
+    lenBotList = len(botList) 
+    text3  = f"**↢ قائمة البوتات - {message.chat.title}**\n\n↢ البوتات\n"
+    while len(botList) > 1:
+      bot = botList.pop(0)
+      text3 += f"├ @{bot.username}\n"    
+    else:    
+      bot = botList.pop(0)
+      text3 += f"└ @{bot.username}\n\n"
+      text3 += f"↢ عدد البوتات : {lenBotList}"  
+      await app.send_message(message.chat.id, text3)
+  except FloodWait as e:
+    await asyncio.sleep(e.value)
+    
+# ------------------------------------------------------------------------------- #
