4f3874eb86a5e14cc8ac9b6bd578908279066153
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-04-22 09:29:03 +0200

    Update صوره+استوري+اقتباس.py

--- iLBaReD/plugins/play/صوره+استوري+اقتباس.py
+++ iLBaReD/plugins/play/صوره+استوري+اقتباس.py
@@ -23,13 +23,29 @@ reply_markup=InlineKeyboardMarkup(
             ]
         )
                            )
-
+                           
+                                   
+@app.on_message(command(["صوره انمي", "انمي"]))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(2,60)
+    url = f"https://t.me/ienamee/{rl}"
+    await client.send_audio(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+                           )
+    
 
 
 
 @app.on_message(command([f"استوريهات", "استوري", "استيت"]))
 async def ihd(client: Client, message: Message):
-    rl = random.randint(2,50)
+    rl = random.randint(2,70)
     url = f"https://t.me/videi_semo//{rl}"
     await client.send_photo(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
 reply_markup=InlineKeyboardMarkup(
@@ -45,8 +61,8 @@ reply_markup=InlineKeyboardMarkup(
 
 @app.on_message(command(["صوره انمي", "انمي"]))
 async def ihd(client: Client, message: Message):
-    rl = random.randint(2,50)
-    url = f"https://t.me/LoreBots7/{rl}"
+    rl = random.randint(2,80)
+    url = f"https://t.me/ienamee/{rl}"
     await client.send_audio(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
     reply_markup=InlineKeyboardMarkup(
             [
@@ -61,7 +77,7 @@ async def ihd(client: Client, message: Message):
                                                               
 @app.on_message(command(["استيكر", "متحركه"]))
 async def ihd(client: Client, message: Message):
-    rl = random.randint(2,50)
+    rl = random.randint(2,90)
     url = f"https://t.me/GifWaTaN/{rl}"
     await client.send_audio(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
     reply_markup=InlineKeyboardMarkup(
@@ -79,7 +95,7 @@ async def ihd(client: Client, message: Message):
     
 @app.on_message(command(["صور بنات", "افاتار بنات"]))
 async def ihd(client: Client, message: Message):
-    rl = random.randint(2,50)
+    rl = random.randint(3,00)
     url = f"https://t.me/vvyuol/{rl}"
     await client.send_audio(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
     reply_markup=InlineKeyboardMarkup(
@@ -95,7 +111,7 @@ async def ihd(client: Client, message: Message):
                             
 @app.on_message(command(["صور شباب", "افاتار شباب"]))
 async def ihd(client: Client, message: Message):
-    rl = random.randint(2,50)
+    rl = random.randint(3,10)
     url = f"https://t.me/vgbmm/{rl}"
     await client.send_audio(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
     reply_markup=InlineKeyboardMarkup(
@@ -111,7 +127,7 @@ async def ihd(client: Client, message: Message):
 
 @app.on_message(command(["سوره", "قرآن"]))
 async def ihd(client: Client, message: Message):
-    rl = random.randint(2,50)
+    rl = random.randint(3,20)
     url = f"https://t.me/opuml/{rl}"
     await client.send_audio(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
     reply_markup=InlineKeyboardMarkup(
@@ -126,7 +142,7 @@ async def ihd(client: Client, message: Message):
                        
 @app.on_message(command(["الشيخ", "النقشبندي", "نقشبندي"]))
 async def ihd(client: Client, message: Message):
-    rl = random.randint(2,50)
+    rl = random.randint(3,30)
     url = f"https://t.me/ggcnjj/{rl}"
     await client.send_audio(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
     reply_markup=InlineKeyboardMarkup(
@@ -138,20 +154,3 @@ async def ihd(client: Client, message: Message):
             ]
         )
                            )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
