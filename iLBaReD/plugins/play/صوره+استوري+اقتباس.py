1cf795e2ae33bf465941371cff7b006a5aaff16d
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-04-22 14:47:27 +0200

    Update صوره+استوري+اقتباس.py

--- iLBaReD/plugins/play/صوره+استوري+اقتباس.py
+++ iLBaReD/plugins/play/صوره+استوري+اقتباس.py
@@ -4,118 +4,133 @@ from strings.filters import command
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from iLBaReD import app
 import random
-from iLBaReD.plugins.play.joinch import ( joinch )
+    
+
+@app.on_message(command([f"صوره", "صورة", "صور"]))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(2,50)
+    url = f"https://t.me/vnnkli/{rl}"
+    await client.send_photo(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",reply_to_message_id=message.id,
+reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+                           )
 
 
+@app.on_message(command([f"استوريهات", "استوري", "استيت"]))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(2,50)
+    url = f"https://t.me/yoipopl{rl}"
+    await client.send_photo(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
+reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+                           )
 
-lisetanme = []  
-@app.on_message(filters.command(["صور انمي", "صورة انمي", "صوره انمي", "انمي"], ""))
-async def sssora(client, message):
-  if not message.chat.type == enums.ChatType.PRIVATE:
-    
-  if len(lisetanme) == 0:
-     user = await get_userbot(client.me.username)
-     async for msg in user.get_chat_history("LoreBots7"):
-      if msg.media:
-        lisetanme.append(msg)
-  phot = random.choice(lisetanme)
-  photo = f"https://t.me/LoreBots7/{phot.id}"
-  await message.reply_photo(photo=photo, caption="**♪ 𝑱𝒐𝒊𝒏 ➧ @Elasyoutyyyy  💎 .**")
 
-lisethazen = []  
-@app.on_message(filters.command(["المزيد من الصور","صور حزينه"], ""))
-async def soorr4(client, message):
-  if not message.chat.type == enums.ChatType.PRIVATE:
+@app.on_message(command(["صوره انمي", "انمي"]))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(2,50)
+    url = f"https://t.me/LoreBots7/{rl}"
+    await client.send_audio(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+                           )
     
-  if len(lisethazen) == 0:
-   user = await get_userbot(client.me.username)
-   async for msg in user.get_chat_history("PVVVV"):
-      if msg.media:
-        lisethazen.append(msg)
-  phot = random.choice(lisethazen)
-  photo = f"https://t.me/PVVVV/{phot.id}"
-  await message.reply_photo(photo=photo, caption="**♪ 𝑱𝒐𝒊𝒏 ➧ @Elasyoutyyyy  💎 .**")
-  
-lisetbnat = []
-@app.on_message(filters.command(["صور بنات", "صورة لبنت", "انمي بنات", "بنات","رمزيات بنات"], ""))
-async def soora4(client, message):
-  if not message.chat.type == enums.ChatType.PRIVATE:
+                                                              
+@app.on_message(command(["استيكر", "متحركه"]))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(2,50)
+    url = f"https://t.me/GifWaTaN/{rl}"
+    await client.send_audio(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+                           )
     
-  if len(lisetbnat) == 0:
-   user = await get_userbot(client.me.username)	
-   async for msg in user.get_chat_history("otsoo3"):
-      if msg.media:
-        lisetbnat.append(msg)
-  phot = random.choice(lisetbnat)
-  photo = f"https://t.me/otsoo3/{phot.id}"
-  await message.reply_photo(photo=photo, caption="**♪ 𝑱𝒐𝒊𝒏 ➧ @Elasyoutyyyy  💎 .**") 
 
-listsoer = []  
-@app.on_message(filters.command(["صور", "صوره", "صورة", "رمزيه", "رمزية", "رمزيات"], ""))
-async def sssor(client, message):
-  if not message.chat.type == enums.ChatType.PRIVATE:
     
-  if len(listsoer) == 0:
-   user = await get_userbot(client.me.username)
-   async for msg in user.get_chat_history("Picture_elnqyb"):
-      if msg.media:
-        listsoer.append(msg)
-  phot = random.choice(listsoer)
-  photo = f"https://t.me/Picture_elnqyb/{phot.id}"
-  await message.reply_photo(photo=photo, caption="**♪ 𝑱𝒐𝒊𝒏 ➧ @Elasyoutyyyy  💎 .**")
-  
-listmu = []
-@app.on_message(filters.command(["اغاني", "غنيلي", "غ", "اغنيه","اغنية عشوائية"], ""))
-async def voece(client, message):
-  if not message.chat.type == enums.ChatType.PRIVATE:
     
-  if len(listmu) == 0:
-   user = await get_userbot(client.me.username)
-   async for msg in user.get_chat_history("ELNQYBMUSIC"):
-      if msg.media:
-        listmu.append(msg.id)
-  audi = random.choice(listmu)
-  audio = f"https://t.me/ELNQYBMUSIC/{audi}"
-  await message.reply_audio(audio=audio, caption="**♪ 𝑱𝒐𝒊𝒏 ➧ @Elasyoutyyyy  💎 .**")
+@app.on_message(command(["صور بنات", "افاتار بنات"]))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(2,50)
+    url = f"https://t.me/vvyuol/{rl}"
+    await client.send_audio(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+                           )
+    
+                            
+@app.on_message(command(["صور شباب", "افاتار شباب"]))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(2,50)
+    url = f"https://t.me/vgbmm/{rl}"
+    await client.send_audio(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+                           )
+    
 
-listvid = []
-@app.on_message(filters.command(["ستوري","استوري","حلات واتس"], ""))
-async def videoo(client, message):
-  if not message.chat.type == enums.ChatType.PRIVATE:
-    
-  if len(listvid) == 0:
-   user = await get_userbot(client.me.username)
-   async for msg in user.get_chat_history("videi_semo"):
-      if msg.video:
-        listvid.append(msg.id)
-  id = random.choice(listvid)
-  video = f"https://t.me/videi_semo/{id}"
-  await message.reply_video(video=video, caption="**♪ 𝑱𝒐𝒊𝒏 ➧ @Elasyoutyyyy  💎 .**")
+@app.on_message(command(["سوره", "قرآن"]))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(2,50)
+    url = f"https://t.me/opuml/{rl}"
+    await client.send_audio(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+                           )
+                       
+@app.on_message(command(["الشيخ", "النقشبندي", "نقشبندي"]))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(2,50)
+    url = f"https://t.me/ggcnjj/{rl}"
+    await client.send_audio(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+                           )
 
-listvidquran = []
-@app.on_message(filters.command(["ستوري قران","استوري قران","حلات واتس قران"], ""))
-async def qurann(client, message):
-  if not message.chat.type == enums.ChatType.PRIVATE:
-    
-  if len(listvidquran) == 0:
-   user = await get_userbot(client.me.username)
-   async for msg in user.get_chat_history("a9li91"):
-      if msg.video:
-        listvidquran.append(msg.id)
-  id = random.choice(listvidquran)
-  video = f"https://t.me/a9li91/{id}"
-  await message.reply_video(video=video, caption="**♪ 𝑱𝒐𝒊𝒏 ➧ @Elasyoutyyyy  💎 .**")
-  
-listmuqurannn = []
-@app.on_message(filters.command(["ق", "قران", "قران كريم", "سوره"], ""))
-async def qurann2(client, message):
-  if not message.chat.type == enums.ChatType.PRIVATE:
-    
-  if len(listmuqurannn) == 0:
-   user = await get_userbot(client.me.username)
-   async for msg in user.get_chat_history("alkoraan4000"):
-      if msg.media:
-        listmuqurannn.append(msg.id)
-  audi = random.choice(listmuqurannn)
-  audio = f"https://t.me/alkoraan4000/{audi}"
-  await message.reply_audio(audio=audio, caption="**♪ 𝑱𝒐𝒊𝒏 ➧ @Elasyoutyyyy  💎 .**")                     
