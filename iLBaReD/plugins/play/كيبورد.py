c56ccf2c3b9397ca416df5e383e716e0bedbbd83
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-04-22 17:03:11 +0200

    Update كيبورد.py

--- iLBaReD/plugins/play/كيبورد.py
+++ iLBaReD/plugins/play/كيبورد.py
@@ -9,26 +9,8 @@ from pyrogram.types import (
     ReplyKeyboardMarkup,
     ReplyKeyboardRemove,
     InputMediaPhoto,
-    Message,
-)
+    Message,)
 from iLBaReD import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-
-
-REPLY_MESSAGE = "صلي علي النبي وتبسم ♥️☺️!"
-
-
-
-
-REPLY_MESSAGE_BUTTONS = [
-[("السورس"),("الاوامر")],
-[("احرف"),("مطور السورس")],
-[("تويت"),("صراحه")],
-[("نكته"),("حكمه")],
-[("انصحني"),("لو خيروك")],
-[("حساب العمر"),("قسم الصور")],
-[("‹ اغلاق الكيبورد ›")]]
-
-
 
 @app.on_message(filters.regex("‹ القائمة الرئيسية ›"), group=39)
 @app.on_message(filters.regex("^/adrenalen"), group=39)
@@ -39,6 +21,40 @@ async def cpanel(_, message: Message):
               text=text,
               reply_markup=reply_markup
         )
+    
+REPLY_MESSAGE = "صلي علي النبي وتبسم ♥️☺️!"
+
+REPLY_MESSAGE_BUTTONS = [
+[("السورس"),("الاوامر")],
+[("احرف"),("مطور السورس")],
+[("تويت"),("صراحه")],
+[("نكته"),("حكمه")],
+[("انصحني"),("لو خيروك")],
+[("حساب العمر"),("قسم الصور")],
+[("‹ قسم الصور ›")]]
+[("‹ اغلاق الكيبورد ›")]]
+
+##############################################################
+
+@app.on_message(filters.regex("‹ قسم الصور ›"), group=40)
+async def cpanel(_, message: Message):             
+        text = REPLY_MESSAGE
+        reply_markup = ReplyKeyboardMarkup(REPLY_MESSAGE_BUTTONS, resize_keyboard=True, selective=True)
+        await message.reply(
+              text=text,
+              reply_markup=reply_markup
+        )
+REPLY_MESSAGE = "- مرحبا بك في قسم الصور ✨♥️ ،"
+
+REPLY_MESSAGE_BUTTONS = [
+[("‹ صوره ›"),("‹ استوري ›")],
+[("‹ استيكر ›"),("‹ انمي ›")],
+[("‹ افتار شباب ›"),("‹ افتار بنات ›")],
+[("‹ النقشبندي ›")],
+[("‹ اغلاق الكيبورد ›")]]
+
+##############################################################
+
 @app.on_message(filters.regex("‹ اغلاق الكيبورد ›"))
 async def down(client, message):
           m = await message.reply("تم اغلاق الكيبورد بنجاح 💘   ⋅ ", reply_markup= ReplyKeyboardRemove(selective=True))
