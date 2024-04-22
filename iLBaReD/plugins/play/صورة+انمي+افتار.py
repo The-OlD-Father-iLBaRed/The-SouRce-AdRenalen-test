cb6cbba76c6cf89a4d290abc5dd8f1d977952fb2
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-04-23 00:49:05 +0200

    Update and rename صوره+استوري+اقتباس.py to صورة+انمي+افتار.py

--- iLBaReD/plugins/play/صورة+انمي+افتار.py
+++ iLBaReD/plugins/play/صورة+انمي+افتار.py
@@ -23,39 +23,6 @@ reply_markup=InlineKeyboardMarkup(
 
 
                        
-@app.on_message(command(["النقشبندي","‹ النخشبندي تواشيح ›"]))
-async def elnaqsbnde(client: Client, message: Message):
-    rl = random.randint(2,51)
-    url = f"https://t.me/ggcnjj/{rl}"
-    await client.send_photo(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
-    reply_to_message_id=message.id,
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        )
-                           )
-
-
-
-@app.on_message(command(["استيت","‹ استوري ›"]))
-async def stats(client: Client, message: Message):
-    rl = random.randint(2,52)
-    url = f"https://t.me/yoipopl{rl}"
-    await client.send_photo(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
-    reply_to_message_id=message.id,
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        )
-                           )
 
 
 @app.on_message(command(["انمي","‹ إنمي ›"]))
@@ -112,35 +79,5 @@ async def avtarto(client: Client, message: Message):
                            )
 
 
-@app.on_message(command(["‹ جيف ›","استيكر"]))
-async def gef(client: Client, message: Message):
-    rl = random.randint(2,55)
-    url = f"https://t.me/GifWaTaN/{rl}"
-    await client.send_photo(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
-    reply_to_message_id=message.id,
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        )
-                           )
 
-
-@app.on_message(command(["سورة عشوائية","‹ قرآن عشوائي ›"]))
-async def Soraa(client: Client, message: Message):
-    rl = random.randint(2,50)
-    url = f"https://t.me/alkoraan4000/{rl}"
-    await client.send_photo(message.chat.id,url,caption="- Join.Channel.SouRce : @WA_ADRENALEN ⋅",
-reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        )
-                           )
 
