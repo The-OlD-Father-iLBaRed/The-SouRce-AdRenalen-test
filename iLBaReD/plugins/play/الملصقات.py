19d723e0d45c95e52e4804e15367f31cd6ad9ef7
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-04-24 23:41:19 +0200

    Update الملصقات.py

--- iLBaReD/plugins/play/الملصقات.py
+++ iLBaReD/plugins/play/الملصقات.py
@@ -17,7 +17,6 @@ from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 from iLBaReD import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from iLBaReD import app
-from iLBaReD import config
 from telegraph import upload_file
 from asyncio import gather
 from pyrogram.errors import FloodWait
