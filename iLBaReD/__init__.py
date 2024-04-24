4b1c4318c66e0b680451f46c721b824eeb282ca5
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-04-24 12:25:30 +0200

    Update __init__.py

--- iLBaReD/__init__.py
+++ iLBaReD/__init__.py
@@ -2,58 +2,25 @@ from iLBaReD.core.bot import Omar
 from iLBaReD.core.dir import dirr
 from iLBaReD.core.git import git
 from iLBaReD.core.userbot import Userbot
-from iLBaReD.misc import dbb, heroku, sudo
-from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from iLBaReD.misc import dbb, heroku
+
 from .logging import LOGGER
 
-# Directories
 dirr()
-
-# Check Git Updates
 git()
-
-# Initialize Memory DB
 dbb()
-
-# Heroku APP
 heroku()
 
-# Load Sudo Users from DB
-sudo()
-
-# Bot Client
 app = Omar()
-
-# Assistant Client
 userbot = Userbot()
+
 
 from .platforms import *
 
-YouTube = YouTubeAPI()
-Carbon = CarbonAPI()
-Spotify = SpotifyAPI()
 Apple = AppleAPI()
-Resso = RessoAPI()
+Carbon = CarbonAPI()
 SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
 Telegram = TeleAPI()
-
-async def AdRenalen_SubScRip(message):
-    if not message.from_user: return
-    try:
-            await message._client.get_chat_member("WA_ADRENALEN", message.from_user.id)
-    except UserNotParticipant:
-                await message.reply(
-                    f"🚦 يجب ان تشترك في القناة\n\nقنـاة الـبـوت : « https://t.me/WA_ADRENALEN »",
-                    disable_web_page_preview=True,
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton("اضـغط هنا للأشتـراك القنـاة 🚦", url=f"https://t.me/WA_ADRENALEN"),
-                            ],
-                         ] 
-                      ) 
-                   )
-                return True
-    except:
-        pass
+YouTube = YouTubeAPI()
