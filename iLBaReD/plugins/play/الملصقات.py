1189f96df0731d3587d204ba182e8ec3f060802a
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-04-24 21:41:19 +0200

    Update الملصقات.py

--- iLBaReD/plugins/play/الملصقات.py
+++ iLBaReD/plugins/play/الملصقات.py
@@ -35,4 +35,7 @@ async def unblock_stickers(client:Client, message:Message):
 async def delete_stickers(client:Client, message:Message):
     if message.chat.id in stiklok:
         await message.delete()
-        await message.reply("لا يمكنك ارسال الملصقات هنا 😋♥️ ، يا  {message.from_user.mention}")
+        await message.reply(f"لا يمكنك ارسال الملصقات هنا 😋♥️ ،[{m.from_user.first_name}](tg://user?id={m.from_user.id}"f")",
+        reply_to_message_id=m.message_id,
+        parse_mode="Markdown")
+        return
