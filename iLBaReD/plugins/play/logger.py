8501a73bea8e6a3e561a05a0f57e4993e059aec9
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-04-25 14:46:09 +0200

    Update logger.py

--- iLBaReD/plugins/play/logger.py
+++ iLBaReD/plugins/play/logger.py
@@ -1,33 +1,71 @@
-from pyrogram.enums import ParseMode
-
 from iLBaReD import app
-from iLBaReD.utils.database import is_on_off
-from config import LOGGER_ID
+from pyrogram import enums
+from pyrogram import Client
+from strings.filters import command
+import asyncio
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from strings.filters import command
+from iLBaReD import app
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram.enums import ChatMemberStatus
 
+stiklok = []
 
-async def play_logs(message, streamtype):
-    if await is_on_off(2):
-        logger_text = f"""
-<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
+@app.on_message(filters.command(["قفل الملصقات","تعطيل الملصقات"],""))
+async def block_stickers(client:Client, message:Message):
+    get = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+        if message.chat.id in stiklok:
+            return await message.reply_text(f"يا {message.from_user.mention} الملصقات مقفلة من قبل")
+        stiklok.append(message.chat.id)
+        return await message.reply_text(f"تم قفل الملصقات \n\n من قبل ←{message.from_user.mention}")
+    else:
+        return await message.reply_text(f"يا {message.from_user.mention} انت لست مشرفا")
 
-<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
-<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
-<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
+@app.on_message(filters.command(["فتح الملصقات","تفعيل الملصقات"],""))
+async def unblock_stickers(client:Client, message:Message):
+    get = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+        if message.chat.id not in stiklok:
+            return await message.reply_text(f"يا {message.from_user.mention} الملصقات مفتوحة من قبل")
+        stiklok.remove(message.chat.id)
+        return await message.reply_text(f"تم فتح الملصقات \n\n من قبل ←{message.from_user.mention}")
+    else:
+        return await message.reply_text(f"يا {message.from_user.mention} انت لست مشرفا")
 
-<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
-<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
-<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
+@app.on_message(filters.sticker)
+async def delete_stickers(client:Client, message:Message):
+    if message.chat.id in stiklok:
+        await message.delete()
+        await message.reply(f"لا يمكنك ارسال الملصقات هنا 😋♥️ ، {message.from_user.mention}")
 
-<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
-        if message.chat.id != LOGGER_ID:
-            try:
-                await app.send_message(
-                    chat_id=LOGGER_ID,
-                    text=logger_text,
-                    parse_mode=ParseMode.HTML,
-                    disable_web_page_preview=True,
-                )
-            except:
-                pass
-        return
+ ##############
+@app.on_message(command(["تخ","بيو"]))
+async def Katl(client: Client, message: Message):
+    if message.reply_to_message.from_user.id == 1924832439:
+        await message.reply("لا يمكنك قتل مطور السورس ي غبي 😋♥️ ،")
+ elif message.reply_to_message.from_user.id == message.from_user.id:
+  await message.reply("لا يمكنك قتل نفسك")
+ elif message.reply_to_message.from_user.id == app.id:
+  await message.reply("لا يمكنك قتل البوت")
+    else:
+        # آيدي الشخص الذي عمل عليه رد الريبلي
+        replied_user_id = message.reply_to_message.from_user.id
+        replied_user_name = message.reply_to_message.from_user.first_name
+        # آيدي الشخص الذي قام بإرسال رد الريبلي
+        killer_id = message.from_user.id
+        killer_name = message.from_user.first_name
+        await message.reply_video(
+            video=f"https://telegra.ph/file/5a18fe591860a8a98f39f.mp4",
+            caption=f"- القاتل المفتري » ⦗ {killer_name} (tg://user?id={killer_id}) ⦘\n- المقتول بمسدس مايه » ⦗ {replied_user_name} (tg://user?id={replied_user_id}) ⦘ \nانا لله وانا اليه راجعون الواد مات بمسدس لعبه 😂!",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "- ضفني في جروبك يرايق 😋♥️ ،", url=f"https://t.me/{app.username}?startgroup=true"),
+                    ],
+                ]
+            ),
+            parse_mode=enums.ParseMode.MARKDOWN
+        )
