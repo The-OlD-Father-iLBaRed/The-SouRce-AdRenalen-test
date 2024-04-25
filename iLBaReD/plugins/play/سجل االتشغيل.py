dca166a11031996617c0a331ed80099fe7b436f1
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-04-25 15:16:11 +0200

    Update سجل االتشغيل.py

--- iLBaReD/plugins/play/سجل االتشغيل.py
+++ iLBaReD/plugins/play/سجل االتشغيل.py
@@ -1,196 +1,86 @@
-from pyrogram.enums import ParseMode
-from iLBaReD import app
-from strings.filters import command
+from pyrogram import Client, filters, idle
 from pyrogram.enums import ChatMemberStatus
-from iLBaReD.utils.database import is_on_off
-from pyrogram import filters
-from config import OWNER_ID
 from pyrogram import Client, filters
-from pyrogram import enums
-from pyrogram.enums import ChatMembersFilter, ChatMemberStatus , ChatType
-from pyrogram.types import ChatPermissions, ChatPrivileges
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import os
 from iLBaReD import app
-import asyncio
+from config import *
 
 
+mutes = []
+@app.on_message(filters.command(["كتم"],"") & filters.group)
+async def mute(app,message):
+   member = await message.chat.get_member(message.from_user.id)
+   if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+     return await message.reply("- يجب ان تكون مشرفا لكتم الاعضاء 💘 ⋅")
+   else:
+     if not message.reply_to_message:
+       return await message.reply("- عليك الرد علي مستخدم وان يكون عضو ليس مشرف 💘 ⋅")
+     member = await message.chat.get_member(message.reply_to_message.from_user.id)
+     if member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+       return await message.reply("- لا يمكنني كتم المشرفين 💘 ⋅")
+     chat_id = str(message.chat.id)
+     user_id = str(message.reply_to_message.from_user.id)
+     x = "{}@{}".format(chat_id,user_id)
+     if x in mutes:
+       return await message.reply("- تم كتم هذا العضو من قبل 💘 ⋅")
+     else:
+       mutes.append(x)
+     return   await message.reply_video(
+        video = "https://telegra.ph/file/9a579139d2171ee0c8d20.mp4",
+        caption = "- العضو ⦗ {} ⦘ ⚡️ ⋅\n- تم كتمة بواسطة ⦗ {} ⦘ ⚡️⋅".format(message.reply_to_message.from_user.mention,message.from_user.mention))
+              
+@app.on_message(filters.command(["الغاء الكتم"],"") & filters.group)
+async def unmute(app,message):
+   member = await message.chat.get_member(message.from_user.id)
+   if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+     return await message.reply("- هذا الامر للمشرفين فقط 💘 ⋅")
+   else:
+     if not message.reply_to_message:
+       return await message.reply("- عليك الرد علي مستخدم وان يكون عضو ليس مشرف 💘 ⋅")
+     member = await message.chat.get_member(message.reply_to_message.from_user.id)
+     if member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+       return await message.reply("- لا يمكنني استعمال الامر علي المشرفين 💘 ⋅")
+     chat_id = str(message.chat.id)
+     user_id = str(message.reply_to_message.from_user.id)
+     x = "{}@{}".format(chat_id,user_id)
+     if not x in mutes:
+       return await message.reply("- تم الغاء كتم المستخدم في هذه المجموعة بنجاح 💘 ⋅")
+     else:
+       mutes.remove(x)
+       return await message.reply("- العضو ⦗ {} ⦘  تم الغاء كتمة بواسطة ⦗ {} ⦘ 💘 ⋅".format(message.reply_to_message.from_user.mention,message.from_user.mention))
+
+@app.on_message(filters.command(["المكتومين"],"")   & filters.group)
+def get_dmute(app, message):
+   if len(mutes) == 0: return
+   member = message.chat.get_member(message.from_user.id)
+   if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+     return message.reply("- هذا الامر للمشرفين فقط 💘 ⋅")
+   ch = message.chat.id
+   c = 0
+   text = "- المكتومين في هذه الـ مجموعة : \n"
+   for a in mutes:
+     chat_id = int(a.split("@")[0])
+     user_id = int(a.split("@")[1])
+     if chat_id == ch:
+        user = app.get_users(user_id)
+        text += f"- {user.mention}\n"
+        c += 1
+   if c == 0: return message.reply("- لا يوجد مكتومين في هذه المجموعة 💘 ⋅")
+   message.reply(f"{text}")
+
+@app.on_message(filters.group)
+def del_msg(_,m):
+   if m.from_user:
+     chat_id = str(m.chat.id)
+     user_id = str(m.from_user.id)
+     x = "{}@{}".format(chat_id,user_id)
+   for a in mutes:
+     if a == x: 
+       m.delete()
+       break
 
 
-
-
-async def play_logs(message, streamtype):
-    if await is_on_off(2):
-        logger_text = f"""
-<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
-
-<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
-<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
-<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
-
-<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
-<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
-<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
-<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
-        if message.chat.id != OWNER_ID:
-            try:
-                await app.send_message(
-                    chat_id=OWNER_ID,
-                    text=logger_text,
-                    parse_mode=ParseMode.HTML,
-                    disable_web_page_preview=True,
-                )
-            except:
-                pass
-        return
-
-
-@app.on_message(filters.command(["صلاحياتي"], ""), group=221213)
-async def caesarprivileges(client, message):
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    cae = await client.get_chat_member(chat_id, user_id)
-    status = cae.status if cae else None
-    if status == ChatMemberStatus.OWNER:
-        await message.reply_text("أنت مالك الجروب")
-    elif status == ChatMemberStatus.MEMBER:
-        await message.reply_text("أنت عضو حقير")
-    else:
-        privileges = cae.privileges if cae else None 
-        can_promote_members = "✅" if (privileges and privileges.can_promote_members) else "❌"
-        can_manage_video_chats = "✅" if (privileges and privileges.can_manage_video_chats) else "❌"
-        can_pin_messages = "✅" if (privileges and privileges.can_pin_messages) else "❌"
-        can_invite_users = "✅" if (privileges and privileges.can_invite_users) else "❌"
-        can_restrict_members = "✅" if (privileges and privileges.can_restrict_members) else "❌"
-        can_delete_messages = "✅" if (privileges and privileges.can_delete_messages) else "❌"
-        can_change_info = "✅" if (privileges and privileges.can_change_info) else "❌"
-        hossam = f"صلاحياتك في الجروب:\n\n"
-        hossam += f"ترقية الأعضاء: {can_promote_members}\n"
-        hossam += f"إدارة الدردشات الصوتية: {can_manage_video_chats}\n"
-        hossam += f"تثبيت الرسائل: {can_pin_messages}\n"
-        hossam += f"دعوة المستخدمين: {can_invite_users}\n"
-        hossam += f"تقييد الأعضاء: {can_restrict_members}\n"
-        hossam += f"حذف الرسائل: {can_delete_messages}\n"
-        hossam += f"تغيير معلومات الجروب: {can_change_info}\n"
-        await message.reply_text(hossam)
-
-
-
-
-
-
-welcome_enabled = True
-
-
-@app.on_chat_member_updated()
-async def welcome(client, chat_member_updated):
-    if not welcome_enabled:
-        return
-    
-    if chat_member_updated.new_chat_member.status == ChatMemberStatus.BANNED:
-        kicked_by = chat_member_updated.new_chat_member.restricted_by
-        user = chat_member_updated.new_chat_member.user
-        
-        if kicked_by is not None and kicked_by.is_self:
-            messagee = f"- المستخدم {user.username} ({user.first_name}) تم طرده من الدردشة بواسطة البوت 💘 ⋅"
-        else:
-            if kicked_by is not None:
-                message = f"منع التصفية التـلقائي 🛡️\n\n- المستخدم : [{kicked_by.first_name}](tg://user?id={kicked_by.id}) نزلته من قائمة الأدمنية\n- السبب : حاول تصفية مجموعتك وطرد العضو : [{user.first_name}](tg://user?id={user.id})"
-                try:
-                    await client.ban_chat_member(chat_member_updated.chat.id, kicked_by.id)
-                except Exception as e:
-                    message += f"\n\n- لازم يكون المشرف مرفوع من البوت عشان أقدر أنزله من الإشراف في حال إذا صفى عضو ⋅\n- لـ معرفة كيفية رفع مشرف : قم بعمل رد على الشخص المحدد واكتب رفع مشرف ⋅"
-            else:
-                message = f"- المستخدم {user.username} ({user.first_name}) - تم طرده من الدردشة ⋅"
-            
-            
-        
-        await client.send_message(chat_member_updated.chat.id, message)
-
-
-
-
-@app.on_message(filters.command(["رفع مشرف"], "") & filters.channel)
-def promote_c_admin(client, message):
-    if message.reply_to_message and message.reply_to_message.from_user:
-        target = message.reply_to_message.from_user.id
-        user_id = str(target)
-    elif message.reply_to_message is None:
-        target = message.text.split()[2]
-        user = app.get_users(target)
-        if user:
-            user_id = str(user.id)
-        else:
-            message.reply_text("- تعزر التعرف علي الحساب هذه ⋅")
-            return
-    else:
-        target = message.text.split()[1].strip("@")
-        user = app.get_users(target)
-        if user:
-            user_id = str(user.id)
-        else:
-            message.reply_text("- تعزر التعرف علي الحساب هذه ⋅")
-            return
-
-    
-    ToM= ChatPrivileges(
-                    can_manage_chat=True,
-                    can_delete_messages=True,
-                    can_manage_video_chats=True,
-                    can_restrict_members=True,
-                    can_promote_members=False,
-                    can_change_info=False,
-                    can_post_messages=True,
-                    can_edit_messages=True,
-                    can_invite_users=True,
-                    can_pin_messages=False,
-                    is_anonymous=False
-                )
-    chat_id = message.chat.id
-    client.promote_chat_member(chat_id, user_id, ToM)
-    message.reply(f"- تم رفع العضو {user_id} صار مشرف ⋅")
-    
-
-
-
-@app.on_message(filters.command(["رفع مشرف"], "") & filters.group)
-def promote_g_admin(client, message):
-    if message.reply_to_message and message.reply_to_message.from_user:
-        target = message.reply_to_message.from_user.id
-        user_id = str(target)
-    elif message.reply_to_message is None:
-        target = message.text.split()[2]
-        user = app.get_users(target)
-        if user:
-            user_id = str(user.id)
-        else:
-            message.reply_text("- لا يمكن العثور على المستخدم ⋅")
-            return
-    else:
-        target = message.text.split()[1].strip("@")
-        user = app.get_users(target)
-        if user:
-            user_id = str(user.id)
-        else:
-            message.reply_text("- تعزر التعرف علي الحساب هذه ⋅")
-            return
-
-    tom_id = message.from_user.id
-    chat_id = message.chat.id
-    ToM= ChatPrivileges(
-                    can_manage_chat=True,
-                    can_delete_messages=True,
-                    can_manage_video_chats=True,
-                    can_restrict_members=True,
-                    can_promote_members=False,
-                    can_change_info=False,
-                    can_post_messages=False,
-                    can_edit_messages=False,
-                    can_invite_users=True,
-                    can_pin_messages=True,
-                    is_anonymous=False
-                )
-    tooom = client.get_chat_members(chat_id, filter=ChatMembersFilter.ADMINISTRATORS)
-    for tom in tooom:
-    	if tom.user.id == tom_id and (tom.status == enums.ChatMemberStatus.OWNER or tom.status == enums.ChatMemberStatus.ADMINISTRATOR):
-    		client.promote_chat_member(chat_id, user_id, ToM)
-    		message.reply(f"- تم رفع العضو {user_id} صار مشرف ⋅")
+app.start()
+print("✓✓✓")
+idle()
