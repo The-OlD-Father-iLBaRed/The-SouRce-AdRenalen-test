f457be7c37494f64970a72c01ace6039e84bcf63
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-04-28 19:36:22 +0300

    Update mongo.py

--- iLBaReD/core/mongo.py
+++ iLBaReD/core/mongo.py
@@ -1,14 +1,14 @@
-from asyncio import AsyncIOMotorClient
+from motor import AsyncIOMotorClient
 
 from config import MONGO_DB_URI
 
 from ..logging import LOGGER
 
-LOGGER("ميــوزك ادرينالين").info("جـارِ الاتصـال بقاعـدة البيانـات . . .")
+LOGGER(__name__).info("Connecting to your Mongo Database...")
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
     mongodb = _mongo_async_.Anon
-    LOGGER("ميــوزك ادرينالين").info("تم الاتصـال بقاعـدة البيانـات ...✓")
+    LOGGER(__name__).info("Connected to your Mongo Database.")
 except:
-    LOGGER(__name__).error("حدث خطأ اثناء الاتصال بقاعدة البيانات.")
+    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
     exit()
