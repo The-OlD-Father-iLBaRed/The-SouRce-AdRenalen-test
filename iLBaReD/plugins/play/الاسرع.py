3a1d698972517ab8d0fa9fee333c78a4394da4d1
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-05-13 19:23:59 +0300

    Update الاسرع.py

--- iLBaReD/plugins/play/الاسرع.py
+++ iLBaReD/plugins/play/الاسرع.py
@@ -5,15 +5,15 @@ from pyrogram.types import Message
 from iLBaReD import app
 import config
 
-
-txt = [
+questions = [
     "- اسرع واحد يدز الكلمة ~ ( بارده)",
     "- اسرع واحد يدز الكلمة ~ ( اجيت)",
-    "**- اسرع واحد يدز الكلمة*ذ ~ ( جبان)",
+    "- اسرع واحد يدز الكلمة ~ ( جبان)",
     "- اسرع واحد يدز الكلمة ~ ( مافهمت)",
     "- اسرع واحد يدز الكلمة ~ ( ميت)",
 ]
-correct_answers = [
+
+answers = [
     "بارده",
     "اجيت",
     "جبان",
@@ -27,19 +27,24 @@ current_question_index = 0
 async def game_handler(client: Client, message: Message):
     global current_question_index
 
-    if current_question_index >= len(correct_answers):
+    if current_question_index >= len(questions):
         await message.reply("تم انتهاء الأسئلة.")
         return
 
-    correct_answer = correct_answers[current_question_index]
+    current_question = questions[current_question_index]
+    correct_answer = answers[current_question_index]
 
-    if message.text.lower() == correct_answer.lower():
+    if message.text.lower() == correct_answer:
         await message.reply("إجابة صحيحة!")
         current_question_index += 1
 
-        if current_question_index < len(correct_answers):
-            await message.reply(f"السؤال الحالي: {correct_answers[current_question_index]}")
+        if current_question_index < len(questions):
+            await message.reply(f"السؤال الحالي: {questions[current_question_index]}")
         else:
             await message.reply("تم انتهاء الأسئلة. شكرًا للمشاركة.")
     else:
         await message.reply("إجابة خاطئة. حاول مرة أخرى.")
+
+        # إرسال سؤال جديد عند الإجابة الخاطئة
+        if current_question_index < len(questions):
+            await message.reply(f"السؤال الحالي: {questions[current_question_index]}")
