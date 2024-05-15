4cec45dfe22bbd5e6e0a60c4967e8fcc4e90da5e
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-05-15 08:49:28 +0300

    Update بوت.py

--- iLBaReD/plugins/play/بوت.py
+++ iLBaReD/plugins/play/بوت.py
@@ -12,25 +12,25 @@ from pyrogram import Client, filters
 from pydub import AudioSegment
 from os import remove
 
-@app.on_message(filters.regex("اكتب")& filters.group)
+@@app.on_message(filters.regex("اكتب")& filters.group)
 async def speech_to_text(client, message):
     if not message.reply_to_message:
-        await message.edit("قم بي الرد علي الصوت اولا")
+        await message.reply("الرد على صوت.")
         return
-    await message.edit("جاري تحميل الصوت")
+    await message.reply("جاري تحميل الصوت")
     voice_down = await message.reply_to_message.download("./recyad.wav")
     voice = sr.Recognizer()
-    await message.edit("جاري استخراج سورس الصوت")
+    await message.reply("جاري استخراج سورس الصوت")
     sound = AudioSegment.from_ogg(voice_down)
     wav_file = sound.export(voice_down, format="wav")
     with sr.AudioFile(wav_file) as source:
         audio_source = voice.record(source)
-    await message.edit("جاري التعرف علي الكلام")
+    await message.reply("جاري التعرف علي الكلام")
     try:
         text = voice.recognize_google(audio_source, language= ar-EG )
     except Exception as e:
         text = f"فشل التعرف علي الكلام\n{e}"
-    await message.edit(text)
+    await message.reply(text)
     remove("recyad.wav")
 async def is_heroku():
     return "heroku" in socket.getfqdn()
