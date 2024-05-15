b2ce43f5d92f783f77a83c3d3723f9d096d8bc89
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-05-15 18:50:07 +0300

    Update بوت.py

--- iLBaReD/plugins/play/بوت.py
+++ iLBaReD/plugins/play/بوت.py
@@ -1,28 +1,38 @@
-import speech_recognition as sr
-from pyrogram import Client, filters
-from pydub import AudioSegment
-from os import remove
+import asyncio
+import random
+from pyrogram import enums
+from pyrogram import types
+from iLBaReD.misc import SUDOERS
+from pyrogram.types import (Message,InlineKeyboardButton,InlineKeyboardMarkup,CallbackQuery,ChatPrivileges)
+from pyrogram import filters, Client
 from iLBaReD import app
+from config import *
+########################################
+Replay_Bot_Meseege = ["اسمي {name} يصحبي 💘 ⋅","يسطا قولتلك اسمي {name } ☺️","اي يزميلي 😂♥️ ،","قلب البوت 🥹💘 ⋅","ثانية بشقط التنية 😂💘 ،","يعم والله بحبك بس ناديلي ب {name} 🙂","اي ي معلم مين مزعلك","ايوا جاااي 😂♥️ ،","تبا لك ماذا تريد من امي 🙂",]
+########################################
+bot_name = {}
+########################################
+name = "ادرينالين"
+########################################
+@app.on_message(filters.regex("تعيين اسم البوت")& filters.private & SUDOERS, group=7113)
+async def set_name_Bot(client, message):
+    if await AdRenalen_SubScRip(message):
+            return
+    global name
+    ask = await app.ask(message.chat.id, "ارسل الاسم الجديد", timeout=30)
+    name = ask.text
+    await message.reply_text("تم تعيين الاسم بنجاح")
+########################################
+@app.on_message(filters.command(["بوت", "البوت"], ""), group=71135)
+async def Bot_Nem_AdRenalen(client, message):
+    global name
+    bot_username = (await app.get_me()).username
+    bar = random.choice(Replay_Bot_Meseege).format(name=name)
+    keyboard = InlineKeyboardMarkup([
+        [InlineKeyboardButton("خدني لجروبك والنبي🥺♥", url=f"https://t.me/{bot_username}?startgroup=True")]])
+    await message.reply_text(
+        text=f"**[{bar}](https://t.me/{bot_username}?startgroup=True)**",
+        disable_web_page_preview=True,
+        reply_markup=keyboard,
+    parse_mode=enums.ParseMode.MARKDOWN)
 
-@app.on_message(filters.regex("اكتب")& filters.group)
-async def speech_to_text(client, message):
-    if not message.reply_to_message:
-        await message.reply("الرد على صوت.")
-        return
-    sent_message = await message.reply("جاري تحميل الصوت")
-    voice_down = await message.reply_to_message.download("./recyad.wav")
-    voice = sr.Recognizer()
-    await sent_message.edit("جاري استخراج سورس الصوت")
-    sound = AudioSegment.from_ogg(voice_down)
-    wav_file = sound.export(voice_down, format="wav")
-    with sr.AudioFile(wav_file) as source:
-        audio_source = voice.record(source)
-    await sent_message.edit("جاري التعرف علي الكلام")
-    try:
-        text = voice.recognize_google(audio_source, language= ar-EG )
-    except Exception as e:
-        text = f"فشل التعرف علي الكلام\n{e}"
-    await message.reply(text)
-    remove("recyad.wav")
-async def is_heroku():
-    return "heroku" in socket.getfqdn()
