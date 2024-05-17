9c04ae5c92efc49606cbf780e31edb0b1c94a0de
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-05-17 03:18:49 +0300

    Update بوت.py

--- iLBaReD/plugins/play/بوت.py
+++ iLBaReD/plugins/play/بوت.py
@@ -1,59 +1,49 @@
 import asyncio
 import random
-from pyrogram import enums
-from pyrogram import types
-from iLBaReD.misc import SUDOERS
-from pyrogram.types import (Message,InlineKeyboardButton,InlineKeyboardMarkup,CallbackQuery,ChatPrivileges)
-from pyrogram import filters, Client
+from pyrogram import enums, filters, Client
+from pyrogram.types import (Message, InlineKeyboardButton, InlineKeyboardMarkup)
 from iLBaReD import app
+from iLBaReD.misc import SUDOERS
 from config import *
-from pyrogram import Client, filters
-from pyrogram.types import Message
-from pymongo import MongoClient
-import re
-from iLBaReD  import app 
-mongo_url_pattern = re.compile(r'mongodb(?:\+srv)?:\/\/[^\s]+')
 
+# قائمة رسائل الرد
+Replay_Bot_Meseege = [
+    "اسمي {name} يصحبي 💘 ⋅",
+    "يسطا قولتلك اسمي {name} ☺️",
+    "اي يزميلي 😂♥️ ،",
+    "قلب البوت 🥹💘 ⋅",
+    "ثانية بشقط التنية 😂💘 ،",
+    "يعم والله بحبك بس ناديلي ب {name} 🙂",
+    "اي ي معلم مين مزعلك",
+    "ايوا جاااي 😂♥️ ،",
+    "تبا لك ماذا تريد من امي 🙂"
+]
 
-@app.on_message(filters.command(["فحص المونجو","فحص"],""))
-async def mongo_command(client, message: Message):
-    if len(message.command) < 2:
-        await message.reply("- استخدم الامر مثل ⦗ فحص + mongodb ⦘")
-        return
+# الاسم الافتراضي
+name = "هههههه"
 
-    mongo_url = message.command[1]
-    if re.match(mongo_url_pattern, mongo_url):
-        try:
-            # Attempt to connect to the MongoDB instance
-            client = MongoClient(mongo_url, serverSelectionTimeoutMS=5000)
-            client.server_info()  # Will cause an exception if connection fails
-            await message.reply("- كود المنجو دا شغال تقدر تستخدمو ✨♥️ ،")
-        except Exception as e:
-            await message.reply(f"- فشل الاتصال السبب : {e}")
-    else:
-        await message.reply("- الكود دا مش شغال مينفعش تستخدمو ✨♥️ ،")
-
-########################################
-Replay_Bot_Meseege = ["اسمي {name} يصحبي 💘 ⋅","يسطا قولتلك اسمي {name } ☺️","اي يزميلي 😂♥️ ،","قلب البوت 🥹💘 ⋅","ثانية بشقط التنية 😂💘 ،","يعم والله بحبك بس ناديلي ب {name} 🙂","اي ي معلم مين مزعلك","ايوا جاااي 😂♥️ ،","تبا لك ماذا تريد من امي 🙂",]
-########################################
-name = ""
-########################################
-@app.on_message(filters.regex("تعيين اسم البوت")& filters.private & SUDOERS, group=7113)
+# دالة لتعيين اسم البوت
+@app.on_message(filters.regex("تعيين اسم البوت") & filters.private & SUDOERS, group=7113)
 async def set_name_Bot(client, message):
     global name
-    neame = await app.ask(message.chat.id, "ارسل الاسم الجديد", filters=filters.text, timeout=30)
-    name = ask.text
+    response = await app.ask(message.chat.id, "ارسل الاسم الجديد", filters=filters.text, timeout=30)
+    name = response.text
     await message.reply_text("تم تعيين الاسم بنجاح")
-########################################
-@app.on_message(filters.command(["بوت", "البوت"], ""), group=71135)
+
+# دالة للرد على الأوامر الخاصة بالبوت
+@app.on_message(filters.command(["بوت", "البوت"], "") & filters.private, group=71135)
 async def Bot_Nem_AdRenalen(client, message):
     global name
     bot_username = (await app.get_me()).username
     bar = random.choice(Replay_Bot_Meseege).format(name=name)
     keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton("خدني لجروبك والنبي🥺♥", url=f"https://t.me/{bot_username}?startgroup=True")]])
+        [InlineKeyboardButton("خدني لجروبك والنبي🥺♥", url=f"https://t.me/{bot_username}?startgroup=True")]
+    ])
     await message.reply_text(
-        text=f"**[{bar}](https://t.me/{bot_username}?startgroup=True)**",
+        text=f"{bar}",
         disable_web_page_preview=True,
         reply_markup=keyboard,
-    parse_mode=enums.ParseMode.MARKDOWN)
+        parse_mode=enums.ParseMode.MARKDOWN
+    )
+
+# داله للدمج و المركدون
