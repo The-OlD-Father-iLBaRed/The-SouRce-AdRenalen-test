da0153f6a774115f86fc85b369cb4e7008bed555
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-05-18 15:29:18 +0300

    Add files via upload

--- iLBaReD/plugins/play/رفع ادمن.py
+++ iLBaReD/plugins/play/رفع ادمن.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram import enums
 import json
-from iLBaReD import app
+from AnonXMusic import app
 from pyrogram.types import (InlineKeyboardButton,
                             InlineKeyboardMarkup, Message)
 from pyrogram import filters, Client
@@ -13,7 +13,10 @@ from pyrogram import filters, Client
 
 
 
-#############               #####                         ###                      ###
+
+
+
+###########               #####                         ###                      ###
       ###                   ##         ##                     ###  ##             ##  ###
       ###                 ##             ##                   ###    ##         ##    ###
       ###               ##                 ##                 ###      ##     ##      ###
@@ -22,10 +25,8 @@ from pyrogram import filters, Client
       ###                      #####                          ###                      ###
 
 
-
-
-@app.on_message(filters.command(["رفع مالك"], ""))
-def promote_owner(client, message):
+@app.on_message(filters.command(["رفع ادمن"], ""))
+def promote_admin(client, message):
     if message.reply_to_message and message.reply_to_message.from_user:
         target = message.reply_to_message.from_user.id
         user_id = str(target)
@@ -47,27 +48,30 @@ def promote_owner(client, message):
             return
 
     chat_id = str(message.chat.id)
-    tom_owners = load_tom_owners()
+    tom_admin = load_tom_admin()
 
-    if (not TOM(client, message, message) and not basic_dev(client, message, message) and not OWNER_ID(client, message, message) and not dev(client, message, message) and not is_basic_creator(client, message, message)):
-        message.reply_text("""◍ انت لست المنشئ الاساسي
+    if (not TOM(client, message, message) and not basic_dev(client, message, message) and not OWNER_ID(client, message, message) and not dev(client, message, message) and not is_basic_creator(client, message, message) and not owner(client, message, message) and not creator(client, message, message)):
+        message.reply_text("""◍ يجب ان تكون منشئ على الاقل لكى تستطيع رفع ادمن
 √""")
         return
-    if chat_id not in tom_owners['owners']:
-        tom_owners['owners'][chat_id] = {'owner_id': []}
 
-    if user_id in tom_owners['owners'][chat_id]['owner_id']:
-        message.reply_text("""◍ هذا المستخدم مالك بالفعل
+    if chat_id not in tom_admin['admin']:
+        tom_admin['admin'][chat_id] = {'admin_id': []}
+
+    if user_id in tom_admin['admin'][chat_id]['admin_id']:
+        message.reply_text("""◍ هذا المستخدم ادمن بالفعل
 √""")
     else:
-        tom_owners['owners'][chat_id]['owner_id'].append(user_id)
-        dump_tom_owners(tom_owners)
-        message.reply_text("""◍ تم رفع المستخدم ليصبح مالك
+        tom_admin['admin'][chat_id]['admin_id'].append(user_id)
+        dump_tom_admin(tom_admin)
+        message.reply_text("""◍ تم رفع المستخدم ليصبح ادمن
 √""")
 
 
-@app.on_message(filters.command(["تنزيل مالك"], ""))
-def demote_owner(client, message):
+
+
+@app.on_message(filters.command(["تنزيل ادمن"], ""))
+def demote_admin(client, message):
     if message.reply_to_message and message.reply_to_message.from_user:
         target = message.reply_to_message.from_user.id
         user_id = str(target)
@@ -87,63 +91,62 @@ def demote_owner(client, message):
         else:
             message.reply_text("لا يمكن العثور على المستخدم")
             return
-
     chat_id = str(message.chat.id)
+    
 
-    tom_owners = load_tom_owners()
-    if (not TOM(client, message, message) and not basic_dev(client, message, message) and not OWNER_ID(client, message, message) and not dev(client, message, message) and not is_basic_creator(client, message, message)):
-        message.reply_text("""◍ انت لست المنشئ الاساسي
+    tom_admin = load_tom_admin()
+    if (not TOM(client, message, message) and not basic_dev(client, message, message) and not OWNER_ID(client, message, message) and not dev(client, message, message) and not is_basic_creator(client, message, message) and not owner(client, message, message) and not creator(client, message, message)):
+        message.reply_text("""◍ يجب ان تكون منشئ على الاقل لكى تستطيع تنزيل ادمن
 √""")
         return
-    if chat_id not in tom_owners['owners']:
-        message.reply_text("لا يوجد مالكين في هذه الدردشة حتى الأن")
+    if chat_id not in tom_admin['admin']:
+        message.reply_text("لا يوجد مشرفين حتى الأن")
         return
 
-    if user_id not in tom_owners['owners'][chat_id]['owner_id']:
-        message.reply_text("""◍ هذا المستخدم ليس مالك لتنزيله
+    if user_id not in tom_admin['admin'][chat_id]['admin_id']:
+        message.reply_text("""◍ هذا المستخدم ليس ادمن لتنزيله
 √""")
     else:
-        tom_owners['owners'][chat_id]['owner_id'].remove(user_id)
-        dump_tom_owners(tom_owners)
-        message.reply_text("""◍ تم تنزيل المستخدم من المالكين بنجاح
+        tom_admin['admin'][chat_id]['admin_id'].remove(user_id)
+        dump_tom_admin(tom_admin)
+        message.reply_text("""◍ تم تنزيل المستخدم من الادمن بنجاح
 √""")
 
 
-@app.on_message(filters.command(["مسح المالكين"], ""))
-def clear_owner(client, message):
+
+@app.on_message(filters.command(["مسح الادمنيه"], ""))
+def clear_admins(client, message):
     chat_id = str(message.chat.id)
-    tom_owners = load_tom_owners()
-    if (not TOM(client, message, message) and not basic_dev(client, message, message) and not OWNER_ID(client, message, message) and not dev(client, message, message) and not is_basic_creator(client, message, message)):
-        message.reply_text("""◍ انت لست المنشئ الاساسي
+    tom_admin = load_tom_admin()
+    if (not TOM(client, message, message) and not basic_dev(client, message, message) and not OWNER_ID(client, message, message) and not dev(client, message, message) and not is_basic_creator(client, message, message) and not owner(client, message, message) and not creator(client, message, message)):
+        message.reply_text("""◍ يجب ان تكون منشئ على الاقل لستخدام الامر
 √""")
         return
-    if chat_id in tom_owners['owners']:
-        tom_owners['owners'][chat_id]['owner_id'] = []
-        dump_tom_owners(tom_owners)
-        message.reply_text("""◍ تم مسح المالكين بنجاح
+    if chat_id in tom_admin['admin']:
+        tom_admin['admin'][chat_id]['admin_id'] = []
+        dump_tom_admin(tom_admin)
+        message.reply_text("""◍ تم مسح الادمنيه بنجاح
 √""")
     else:
-        message.reply_text("لا يوجد مالكين ليتم مسحهم")
+        message.reply_text("لا يوجد ادمنيه ليتم مسحهم")
 
 
 
 
 
-@app.on_message(filters.command(["المالكين"], ""))
-def get_owner(client, message):
+
+@app.on_message(filters.command("الادمنيه", ""))
+def get_admins(client, message):
     chat_id = str(message.chat.id)
-    tom_owners = load_tom_owners()
-    if (not TOM(client, message, message) and not basic_dev(client, message, message) and not OWNER_ID(client, message, message) and not dev(client, message, message) and not is_basic_creator(client, message, message) and not owner(client, message, message)):
-        message.reply_text("""◍ يجب ان تكون مالك على الاقل لستخدام الامر
-√""")
-        return
-    if chat_id not in tom_owners['owners']:
-        message.reply_text("لا يوجد مالكين حتى الأن")
+    tom_admin = load_tom_admin()
+
+    if chat_id not in tom_admin['admin']:
+        message.reply_text("لا يوجد مشرفين في هذه الدردشة")
         return
 
-    admins = tom_owners['owners'][chat_id]['owner_id']
+    admins = tom_admin['admin'][chat_id]['admin_id']
     if not admins:
-        message.reply_text("لا يوجد مالكين حتى الأن")
+        message.reply_text("لا يوجد مشرفين في هذه الدردشة")
     else:
         admin_names = []
         for admin_id in admins:
@@ -151,12 +154,12 @@ def get_owner(client, message):
             if user:
                 admin_names.append(f"[{user.first_name}](tg://user?id={user.id})")
 
+        
         if admin_names:
             admin_list = "\n".join(admin_names)
-            message.reply_text(f"◍ قائمة المالكين:\n\n{admin_list}")
+            message.reply_text(f"◍ قائمة المشرفين:\n\n{admin_list}")
         else:
-            message.reply_text("تعذر العثور على معلومات المالكين")
-
+            message.reply_text("تعذر العثور على معلومات المشرفين")
 
 
 #############               #####                         ###                      ###
